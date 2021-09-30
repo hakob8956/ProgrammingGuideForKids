84800b1d191edd2f4c3c7cfbbc53b5258cbd6018
Hakob Nersesyan <Hakob.Nersesyan@volo.global>
2021-09-30 18:55:22 +0400

    add:utility which contain function helpers

---
+++ utility.js
@@ -0,0 +1,16 @@
+var utility = {
+    getRandomInt: function(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min + 1;
+    },
+    shuffleArray: function(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    },
+};
